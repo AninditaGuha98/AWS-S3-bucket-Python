451ff3c15858551f82a1fb2b873732a6c8b34dc7
guha <an711755@dal.ca>
2020-05-22 21:02:20 -0300

    Part B done

--- Assignment-1/AWS_python.py
+++ Assignment-1/AWS_python.py
@@ -1,28 +1,43 @@
 author="Anindita"
 
 import boto3
-import boto
-#Linking with the client type
-s3=boto3.client('s3')
+s3=boto3.client('s3')   #Linking with the client type
+# s3.upload_file("anindita.txt", "anindita-bucket","anindita.txt")    #Uploading file to s3 bucket, format: Filepath, bucket-name, filename
+# s3.create_bucket(Bucket='anindita-third-bucket')   #Creating second bucket
 
-#Uploading file to s3 bucket, format: Filepath, bucket-name, filename
-# s3.upload_file("anindita.txt", "anindita-bucket","anindita.txt")
 
-#Created second bucket
-# s3.create_bucket(Bucket='anindita-second-bucket')
-
-#part B
-client=boto3.client('s3')
-# response = client.create_access_point(
-#     AccountId='998882546124',
-#     Name='PartB',
-#     Bucket='anindita-second-bucket',
+# #Changing public access permissions
+# client=boto3.client('s3')
+# response = client.put_public_access_block(
+# Bucket='anindita-second-bucket',
 #     PublicAccessBlockConfiguration={
 #         'BlockPublicAcls': True,
-#         'IgnorePublicAcls': False,
-#         'BlockPublicPolicy': False,
-#         'RestrictPublicBuckets': False
+#         'IgnorePublicAcls': True,
+#         'BlockPublicPolicy': True,
+#         'RestrictPublicBuckets': True
 #     }
 # )
-response=client.put_bucket_acl(Bucket='anindita-second-bucket')
-print(response)
+
+#Disabling ACL write permission
+# response=s3.get_bucket_acl(Bucket='anindita-second-bucket')
+# s3.put_bucket_acl(
+#     AccessControlPolicy={
+#         'Grants': [
+#             {
+#                 'Grantee': {
+#                     'DisplayName': 'awslabsc0w516804t1575138305',
+#                     'ID': 'a8e938119a91aa2be92ca36e610229629bc0b265b354c5f068d5e21beb3b9439',
+#                     'Type': 'CanonicalUser',
+#                 },
+#                 'Permission': 'READ_ACP'
+#             },
+#         ],
+#         'Owner': {
+#             'DisplayName': 'awslabsc0w516804t1575138305',
+#             'ID': 'a8e938119a91aa2be92ca36e610229629bc0b265b354c5f068d5e21beb3b9439'
+#         }
+#     },
+#     Bucket='anindita-second-bucket'
+# )
+
+#Move File from 1st bucket to 2nd bucket
