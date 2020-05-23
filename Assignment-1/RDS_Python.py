4ab6cf5201689ec25e9f0d348a1b423f56710482
guha <an711755@dal.ca>
2020-05-23 04:43:43 -0300

    Part C final

--- Assignment-1/RDS_Python.py
+++ Assignment-1/RDS_Python.py
@@ -9,56 +9,65 @@ conn = mysql.connector.connect(host='serverless-2.ccmdqmbvy12j.us-east-1.rds.ama
                                db='login_database')
 
 cursor = conn.cursor()
-# Inserting Value into database
-
-id=input("Please enter the user id: ")
-password=input("Please enter the password: ")
+LookUp={}
 
 # sending file to S3 bucket
-#
-# s3=boto3.client('s3')
-# s3.upload_file("Lookup5410.txt", "anindita-bucket","Lookup5410.txt")
-#
-# s=boto3.resource('s3')
-# #fetching file from s3 bucket
-# s.Bucket('anindita-bucket').download_file("Lookup5410.txt",'Lookup.txt')
+def send_fetchS3():
+    s3=boto3.client('s3')
+    s3.upload_file("Lookup5410.txt", "anindita-bucket","Lookup5410.txt")
+    s=boto3.resource('s3')
+    #fetching file from s3 bucket
+    s.Bucket('anindita-bucket').download_file("Lookup5410.txt",'Lookup.txt')
+    readlookup()
+
+#Reading the lookup text file
+def readlookup():
+    with open('Lookup.txt', 'r') as read:
+        for i in read.readlines():
+            LookUp[i.split()[0]] = i.split()[1]
+    database_insert(cursor)
 
 
-#
-LookUp={}
-encryptedpass=''
-with open('Lookup.txt','r') as read:
-    lookup=read.readline().split()
-    for i in read.readlines():
-        LookUp[i.split()[0]]=i.split()[1]
+#Function for encrypting the password
+def passwordEncryption(password):
+    encryptedpass=''
     for letter in password:
         encryptedpass=encryptedpass+''+LookUp[letter]
-print(encryptedpass)
+    return encryptedpass
 
 
-insert_data=("INSERT into login (userId,password) values(%s,%s)")
-cursor.execute(insert_data,(id,encryptedpass))
+#Function for decrypting the password
+def passwordDecryption(results):
+    decryptedpass=''
+    word = list(results[1])
+    it = iter(word)
+    for i in it:
+        match=i+''+ next(it)
+        for key,value in LookUp.items():
+            if value==match:
+                decryptedpass =decryptedpass+''+ key
+                break
+    return decryptedpass
+
+# Inserting values into database
+def database_insert(cursor):
+    id=input("Please enter the user id: ")
+    password=input("Please enter the password: ")
+    encryptedpass=passwordEncryption(password)
+    insert_data = ("INSERT into login (userId,password) values(%s,%s)")
+    cursor.execute(insert_data, (id, encryptedpass))
+    databaseRetrieve(cursor)
 
 
 # Fetching value from database
-fetch_id=int(input("id?"))
-fetch_value = ("Select * from login where userId={0}".format(fetch_id))
-cursor.execute(fetch_value)
-results = cursor.fetchone()
-decryptedpass=''
+def databaseRetrieve(cursor):
+    fetch_id=int(input("Which id do you want to fetch?"))
+    fetch_value = ("Select * from login where userId={0}".format(fetch_id))
+    cursor.execute(fetch_value)
+    results = cursor.fetchone()
+    decryptedpass=passwordDecryption(results)
+    print("Decrypted password: ",decryptedpass)
 
-word = list(results[1])
-it = iter(word)
-print(word)
-for i in it:
-    match=i+''+ next(it)
-
-    for key,value in LookUp.items():
-        if value==match:
-            decryptedpass=decryptedpass+''+ key
-            break
-print(decryptedpass)
-
-
+send_fetchS3()
 conn.commit()
 conn.close()
